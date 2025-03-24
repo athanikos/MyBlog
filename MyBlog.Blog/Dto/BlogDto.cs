1f30167885452bb7e54e0c36e7b5ab09b99b389a
Nikos Athanasakis <athanikos@gmail.com>
2025-03-24 21:28:29 +0200

    Blog draft and dto

---
+++ MyBlog.Blog/Dto/BlogDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MyBlog.Blog.Dto
+{
+    public class BlogDto
+    {
+        public Guid Id { get; set; }
+
+        public required string ShortTitle { get; set; }
+
+        public required string LargeTitle { get; set; }
+
+        public required string Body { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+    }
+}
---
+++ MyBlog.Blog/Entities/Blog.cs
@@ -0,0 +1,15 @@
+﻿namespace MyBlog.Blog.Entities
+{
+    public class Blog 
+    {
+        public Guid Id { get; set; }
+
+        public required string ShortTitle { get; set; }
+
+        public required string LargeTitle { get; set; }
+
+        public required string Body { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+    }
+}
